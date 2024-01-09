4408302e35400ac5122732c3b0861c8f65c62fb5
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-01-09 11:47:06 +0330

    product model

--- products/models.py
+++ products/models.py
@@ -3,9 +3,16 @@ from base.models import BaseModel
 
 # Create your models here.
 
-class Category(models.Model):
+class Category(BaseModel):
     category_name = models.CharField(max_length=100)
     category_image = models.ImageField(upload='categories')
     
     def __str__(self):
         return self.category_name
+
+
+class Product(BaseModel):
+    product_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.IntegerField()
+    
