1ad258c43c0a7e86e9773c517ba7070407f7e3f4
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-01-09 11:32:12 +0330

    Category model

--- products/models.py
+++ products/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from base.models import BaseModel
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    category_image = models.ImageField(upload='categories')
+    
+    def __str__(self):
+        return self.category_name
