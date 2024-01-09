9b23b96d3a195e834ef71f7b3334055b9c81d362
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-01-09 13:23:11 +0330

    profile model

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,11 @@ from base.models import BaseModel
 
 # Create your models here.
 
-
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    is_email_verified = models.BooleanField(default=False)
+    email_token = models.CharField(max_length=100, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile')
+    
+    def __str__(self):
+        return self.user
