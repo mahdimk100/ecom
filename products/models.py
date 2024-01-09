e3b1b0976588be026322c795c150fc1b7f5082fa
mahdi khodaei <109039802+mahdimk100@users.noreply.github.com>
2024-01-09 12:08:37 +0330

    ProduactImage model

--- products/models.py
+++ products/models.py
@@ -15,4 +15,12 @@ class Product(BaseModel):
     product_name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.IntegerField()
+    product_description = models.TextField()
     
+    def __str__(self):
+        return self.product_name
+    
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image')
+    image = models.ImageField(upload='product')
